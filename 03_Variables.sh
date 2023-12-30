390643e8aecf8c5c3ce95df460c65a5b4e896889
venkatparsapu <venkatparsapu123@gmail.com>
2023-12-30 19:39:53 +0530

    vars declarations multiple

--- 03_Variables.sh
+++ 03_Variables.sh
@@ -5,3 +5,5 @@ PERSON2=Ravi
 
 echo "$PERSON1: Hi Ravi how r you"
 echo "$PERSON2: Hi Suresh i am good and how r you"
+echo "$PERSON1: I am also good what are the weekend plans"
+echo "$PERSON2: watching movies"
