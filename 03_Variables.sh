981d2ea6f17eaed993bccf140f617515f1b3efeb
venkatparsapu <venkatparsapu123@gmail.com>
2023-12-30 19:42:41 +0530

    vars declarations multiple

--- 03_Variables.sh
+++ 03_Variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PERSON1=Suresh
-PERSON2=Ravi
+PERSON1=Ram
+PERSON2=Sai
 
-echo "$PERSON1: Hi Ravi how r you"
-echo "$PERSON2: Hi Suresh i am good and how r you"
+echo "$PERSON1: Hi $PERSON2 how r you"
+echo "$PERSON2: Hi $PERSON1 i am good and how r you"
 echo "$PERSON1: I am also good what are the weekend plans"
 echo "$PERSON2: watching movies"
