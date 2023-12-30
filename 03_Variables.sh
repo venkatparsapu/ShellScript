5755fa5a91991bbb7cc8a11c3d6a9fe2cc88186e
venkatparsapu <venkatparsapu123@gmail.com>
2023-12-30 19:47:20 +0530

    vars declarations multiple date

--- 03_Variables.sh
+++ 03_Variables.sh
@@ -7,3 +7,7 @@ echo "$PERSON1: Hi $PERSON2 how r you"
 echo "$PERSON2: Hi $PERSON1 i am good and how r you"
 echo "$PERSON1: I am also good what are the weekend plans"
 echo "$PERSON2: watching movies"
+
+DATE=$(date)
+
+echo "Time and Date is : $DATE"
