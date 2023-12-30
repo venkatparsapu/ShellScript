53c0d96e590355a0953c4cd2bb73b66f416be51d
venkatparsapu <venkatparsapu123@gmail.com>
2023-12-30 19:37:51 +0530

    vars declaration

--- 03_Variables.sh
+++ 03_Variables.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "Suresh: Hi Ravi how r you"
-echo "Ravi: Hi Suresh i am good and how r you"
+PERSON1=Suresh
+PERSON2=Ravi
+
+echo "$PERSON1: Hi Ravi how r you"
+echo "$PERSON2: Hi Suresh i am good and how r you"
